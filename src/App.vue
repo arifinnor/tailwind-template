8ef58f095a31026172e54642e68a179e405a0c83
M Arifin Nor <arifinnor03@gmail.com>
2022-08-18 15:05:57 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import Login from './components/Login.vue';
+
+</script>
+
+<template>
+  <Login />
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<script setup>
+  const form = {
+    email: '',
+    password: '',
+  };
+</script>
+
+<template>
+
+<div class="">
+  <div class="w-full h-full">
+    <form>
+      <div class="mb-6">
+        <label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+        <input v-model="form.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="email" placeholder="Username">
+      </div>
+      <div class="mb-6">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+        <input v-model="form.password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" id="password" placeholder="Password">
+
+      </div>
+
+      <button type="submit">Login</button>
+
+    </form>
+  </div>
+
+</div>
+  
+</template>
